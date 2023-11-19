8ef3f98a4f7e341ccd18335f7003a210ce95620b
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-11-19 20:01:33 +0300

    add steps

---
+++ app/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Steps from "./Steps";
+
+export default function Main() {
+  return (
+    <div>
+      <nav className="p-2 shadow-md flex flex-col sm:flex-row sm:items-center sm:justify-between">
+        <div className="font-semibold text-xl md:text-2xl">KurdLex</div>
+        <div className="flex items-center mt-1">
+          <input
+            type="text"
+            placeholder="Search for words"
+            className="outline outline-1 rounded-md p-1 outline-indigo-500 w-full sm:w-64 md:w-96"
+          />
+          <button className="bg-indigo-500 text-white rounded-md p-1 ml-1">
+            Search
+          </button>
+        </div>
+      </nav>
+      <Steps />
+    </div>
+  );
+}
---
+++ app/components/Steps.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+export default function Steps() {
+  return (
+    <div className="flex flex-col sm:flex-row">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              1
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Shooting Stars
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              2
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  The Catalyzer
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font" dir="rtl">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              1
+            </div>
+            <div className="flex-grow md:pr-8 pr-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pr-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Shooting Stars
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              2
+            </div>
+            <div className="flex-grow md:pr-8 pr-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pr-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  The Catalyzer
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
