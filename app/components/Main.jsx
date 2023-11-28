f269d6257aea3af19cb83b3fce8430f4eba6a06b
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-11-28 16:20:00 +0300

    improve folder

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,10 +6,13 @@ import axios from "axios";
 
 export default function Main() {
   const [data, setData] = useState(null);
-  const handleFetch = async () => {
+
+  const handleFetch = async (word) => {
+    lo;
+
     try {
       const response = await axios.get(
-        "http://localhost:3000/api/scrape?word=cold"
+        `http://localhost:3000/api/scrape?word=${word}`
       );
       const data = response.data;
 
@@ -19,22 +22,22 @@ export default function Main() {
       console.error("Error fetching data:", error.message);
     }
   };
+
   return (
     <div>
       <nav className="p-2 shadow-md flex flex-col sm:flex-row sm:items-center sm:justify-between">
         <div className="font-semibold text-xl md:text-2xl">KurdLex</div>
         <div className="flex items-center mt-1">
-          <input
-            type="text"
-            placeholder="Search for words"
-            className="outline outline-1 rounded-md p-1 outline-indigo-500 w-full sm:w-64 md:w-96"
-          />
-          <button
-            onClick={handleFetch}
-            className="bg-indigo-500 text-white rounded-md p-1 ml-1"
-          >
-            Search
-          </button>
+          <form onSubmit={handleFetch}>
+            <input
+              type="text"
+              placeholder="Search for words"
+              className="outline outline-1 rounded-md p-1 outline-indigo-500 w-full sm:w-64 md:w-96"
+            />
+            <button className="bg-indigo-500 text-white rounded-md p-1 ml-1">
+              Search
+            </button>
+          </form>
         </div>
       </nav>
       <Steps />
