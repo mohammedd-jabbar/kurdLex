c71d81bf376a4a309839ef966c67530e4872e3f6
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-08 10:09:04 +0300

    add word origin

--- app/components/Steps.jsx
+++ app/components/Steps.jsx
@@ -12,52 +12,96 @@ export default function Steps() {
   const numberOfSteps = Math.min(numberOfDefinitions, 3);
   return (
     data && (
-      <div className="flex flex-col sm:flex-row" id="dictionary">
-        <section className="text-gray-600 body-font" dir="ltr">
-          <div className="container px-2 py-24 mx-auto flex flex-wrap">
-            {Array.from({ length: numberOfSteps }, (_, index) => (
-              <div
-                key={index}
-                className="flex relative pt-10 pb-16 sm:items-center md:w-2/3 mr-[15%] md:mx-auto"
-              >
-                <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                </div>
-                <div className="flex-shrink-0 w-7 h-7 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
-                  <CgSearchLoading className="w-[1.15rem] h-[1.15rem]" />
-                </div>
-                <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                  <StepsData data={data} i={index} first={index === 0} />
-                </div>
-              </div>
-            ))}
-          </div>
-        </section>
-
-        {/* Kurdish steps */}
-        <section className="text-gray-600 body-font" dir="rtl">
-          <div className="container px-2 py-24 mx-auto flex flex-wrap">
-            {Array.from({ length: numberOfSteps }, (_, index) => (
-              <div
-                key={index}
-                className="flex relative pt-10 pb-16 sm:items-center md:w-2/3 ml-[15%] md:mx-auto"
-              >
-                <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                </div>
-                <div className="flex-shrink-0 w-7 h-7 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
-                  <CgSearchLoading className="w-[1.15rem] h-[1.15rem]" />
-                </div>
+      <div>
+        <div className="flex flex-col sm:flex-row" id="dictionary">
+          {/* English steps */}
+          <section className="text-gray-600 body-font" dir="ltr">
+            <div className="container px-2 py-20 mx-auto flex flex-wrap">
+              {Array.from({ length: numberOfSteps }, (_, index) => (
                 <div
-                  className="flex-grow md:pr-8 pr-6 flex sm:items-center items-start flex-col sm:flex-row"
-                  dir="rtl"
+                  key={index}
+                  className="flex relative pt-10 pb-16 sm:items-center md:w-2/3 mr-[15%] md:mx-auto"
                 >
-                  <StepsData data={data} i={index} first={index === 0} />
+                  <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  </div>
+                  <div className="flex-shrink-0 w-7 h-7 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+                    <CgSearchLoading className="w-[1.15rem] h-[1.15rem]" />
+                  </div>
+                  <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+                    <StepsData data={data} i={index} first={index === 0} />
+                  </div>
                 </div>
+              ))}
+            </div>
+          </section>
+
+          {/* Kurdish steps */}
+          <section className="text-gray-600 body-font" dir="rtl">
+            <div className="container px-2 py-20 mx-auto flex flex-wrap">
+              {Array.from({ length: numberOfSteps }, (_, index) => (
+                <div
+                  key={index}
+                  className="flex relative pt-10 pb-16 sm:items-center md:w-2/3 ml-[15%] md:mx-auto"
+                >
+                  <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  </div>
+                  <div className="flex-shrink-0 w-7 h-7 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+                    <CgSearchLoading className="w-[1.15rem] h-[1.15rem]" />
+                  </div>
+                  <div
+                    className="flex-grow md:pr-8 pr-6 flex sm:items-center items-start flex-col sm:flex-row"
+                    dir="rtl"
+                  >
+                    <StepsData data={data} i={index} first={index === 0} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
+        <hr className="w-[80%] mb-6 mx-auto h-[1px] bg-gray-300 dark:bg-white/40 border-none" />
+
+        {/* Word Origin */}
+        <div className="flex w-[80%] mx-auto mb-10 space-x-8">
+          <div className="text-center">
+            <div className="flex flex-col bg-gray-100 border border-gray-200 dark:bg-gray-500 dark:border-gray-700 shadow rounded-md">
+              <div className="flex-grow pl-5 pr-2 rtl:pr-5 rtl:pl-2 mt-6 sm:mt-4 mb-6 dark:text-white">
+                {data && (
+                  <>
+                    <div className="mt-2">
+                      <div className="mb-4">
+                        <h1 className="font-bold mb-1">WORD ORIGIN</h1>
+                        <p className="leading-relaxed pl-1.5 text-gray-700 dark:text-white/90">
+                          {data.wordOrigin}
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                )}
               </div>
-            ))}
+            </div>
           </div>
-        </section>
+          <div className="text-center">
+            <div className="flex flex-col bg-gray-100 border border-gray-200 dark:bg-gray-500 dark:border-gray-700 shadow rounded-md">
+              <div className="flex-grow pl-5 pr-2 rtl:pr-5 rtl:pl-2 mt-6 sm:mt-4 mb-6 dark:text-white">
+                {data && (
+                  <>
+                    <div className="mt-2">
+                      <div className="mb-4">
+                        <h1 className="font-bold mb-1">WORD ORIGIN</h1>
+                        <p className="leading-relaxed pl-1.5 text-gray-700 dark:text-white/90">
+                          {data.wordOrigin}
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   );
