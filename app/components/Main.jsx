aa44b8a4fc53db3398ac8edf92963a493ddf3801
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-09 10:01:12 +0300

    add translate route

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -127,23 +127,6 @@ export default function Main() {
                   entries.
                 </p>
               </div>
-              {!isSubmitState && (
-                <a
-                  href="#dictionary"
-                  aria-label="Scroll down"
-                  className="flex items-center justify-center w-10 h-10 mx-auto text-gray-600 duration-300 transform border border-gray-400 rounded-full hover:text-deep-purple-accent-400 hover:border-deep-purple-accent-400 hover:shadow hover:scale-110"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="currentColor"
-                  >
-                    <path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z" />
-                  </svg>
-                </a>
-              )}
             </div>
           </form>
           <Steps />
