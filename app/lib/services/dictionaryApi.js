4e15c7ccdbe427426efc4a46744a76cc06dd987a
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-14 11:26:57 +0300

    add urls

--- app/lib/services/dictionaryApi.js
+++ app/lib/services/dictionaryApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const dictionaryApi = createApi({
   reducerPath: "dictionaryApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://sweet-seahorse-2c570a.netlify.app",
+    baseUrl: "https://kurd-gde45yoes-mohammeds-projects-004ceb9e.vercel.app",
   }),
 
   endpoints: (builder) => ({
