58c3321344431240972be7b08831642e5e49d2ca
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-16 12:33:30 +0300

    add some config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require("next-intl/plugin")("./i18n.js");
 
-module.exports = withNextIntl({});
+module.exports = withNextIntl({
+  cleanDistDir: false,
+});
