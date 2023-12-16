49c483f347b0600b1892e21ce364e0df7268c8e9
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-16 19:43:50 +0300

    remove spell checking

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require("next-intl/plugin")("./i18n.js");
 
-module.exports = withNextIntl({
-  cleanDistDir: false,
-});
+module.exports = withNextIntl({});
