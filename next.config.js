eaf51b7381977039bda9187b940977b16b63137c
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-17 06:53:14 +0300

    fix webpack load file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require("next-intl/plugin")("./i18n.js");
 
-module.exports = withNextIntl({});
+module.exports = withNextIntl({
+  webpack: (config) => {
+    // Add a rule to handle .aff and .dic files
+    config.module.rules.push({
+      test: /\.(aff|dic)$/,
+      use: "file-loader",
+    });
+
+    return config;
+  },
+});
